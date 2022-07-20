86d75b647ad57743b5388754b57e09c63a31cf48
nnamaka <nnamaka7@gmail.com>
2022-07-20 23:47:43 +0000

    microservice

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=flaskapp
+docker build --tag=flaskapp .
 
 # Step 2: 
 # List docker images
@@ -13,3 +13,4 @@ docker image ls
 # Step 3: 
 # Run flask app
 docker run -it flaskapp bash
+docker run -p 8000:80 api
