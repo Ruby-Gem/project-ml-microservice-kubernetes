fa99d632e85ce8c35a16ca9cacdcdc82429dd408
EC2 Default User <ec2-user@ip-172-31-28-158.us-east-2.compute.internal>
2022-07-21 16:16:21 +0000

    microservice

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="nnamaka/python-helloworld:flaskappV1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskappone\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskappV1.0.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskappone 8000:80
