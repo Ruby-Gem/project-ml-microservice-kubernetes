a160212eba45150bc3d30de8fb8a00bd51157c30
nnamaka <nnamaka7@gmail.com>
2022-07-21 12:14:58 +0000

    microservice

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="nnamaka/python-helloworld"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag flaskapp nnamaka/python-helloworld:flaskappV1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker push nnamaka/python-helloworld:flaskappV1.0.0
